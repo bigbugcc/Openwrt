893b0fbcfa204a477d77712ade9acab2b79ebe10
bigbug <ibigbugcc@gmail.com>
2021-02-05 21:15:30 +0800

    1.2.2

--- openwrtinstall.sh
+++ openwrtinstall.sh
@@ -14,37 +14,40 @@ fi
 
 cd $project_path &&
 
-if [ -f "$project_path/make.sh" ];then 
-        mv ./make.sh ./lede
-    else
-        if [ -f "$project_path/lede/make.sh" ];then 
-                echo "搜索到make.sh文件，正在执行！"
-                chmod +x $project_path/lede/make.sh 
-            else
-                echo "错误！不存在make.sh文件，将不会自动编译固件" 
-    fi
-fi
-
-#otherapp目录
+#otherapp插件目录
 if [ -d "$project_path/lede/package/otherapp" ];then
         echo "***otherapp已存在***"
     else
         mkdir "$project_path/lede/package/otherapp"
+        cd $project_path/lede/package/otherapp
+		#在线用户
+		git clone https://github.com/rufengsuixing/luci-app-onliner
+		#科学上网插件
+		git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall
+		git clone --depth=1 -b master https://github.com/vernesong/OpenClash
+		#大学生必备插件
+		git clone --depth=1 https://github.com/BoringCat/luci-app-mentohust
+		git clone --depth=1 https://github.com/KyleRicardo/MentoHUST-OpenWrt-ipk 
+		#广告过滤插件
+		git clone https://github.com/rufengsuixing/luci-app-adguardhome 
+		git clone https://github.com/destan19/OpenAppFilter  #(luci-app-flowoffload shortcut-fe luci-app-sfe)
+		#Lienol App
+		git clone https://github.com/Lienol/openwrt-package
+		cd ../../../
+		echo "---------- $(cd `dirname $0`; pwd)"
 fi
+echo "---------- $(cd `dirname $0`; pwd)"
 
-cd $project_path/lede/package/otherapp &&
-#在线用户
-git clone https://github.com/rufengsuixing/luci-app-onliner &&
-#科学上网插件
-git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall &&
-git clone --depth=1 -b master https://github.com/vernesong/OpenClash &&
-#大学生必备插件
-git clone --depth=1 https://github.com/BoringCat/luci-app-mentohust &&
-git clone --depth=1 https://github.com/KyleRicardo/MentoHUST-OpenWrt-ipk &&
-#广告过滤插件
-git clone https://github.com/rufengsuixing/luci-app-adguardhome &&
-git clone https://github.com/destan19/OpenAppFilter && #(luci-app-flowoffload shortcut-fe luci-app-sfe)
-#Lienol App
-git clone https://github.com/Lienol/openwrt-package &&
-
-cd ../../ && "$(cd `dirname $0`; pwd)"/make.sh
+if [ -f "$project_path/make.sh" ];then 
+		chmod +x ./make.sh
+    	mv ./make.sh ./lede
+    	$project_path/lede/make.sh
+    else
+        if [ -f "$project_path/lede/make.sh" ];then 
+                echo "搜索到make.sh文件，正在执行！"
+                chmod +x $project_path/lede/make.sh
+                "$(cd `dirname $0`; pwd)"/lede/make.sh 
+            else
+                echo "错误！不存在make.sh文件，将不会自动编译固件" 
+    	fi
+fi
