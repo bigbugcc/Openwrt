965640dfa2fc9c14f401fa2073ee680285f7957b
bigbugcc <1373396036@qq.com>
2021-06-01 12:17:06 +0800

    Add R1Plus

--- openwrtinstall.sh
+++ openwrtinstall.sh
@@ -5,7 +5,7 @@ project_path=$(cd `dirname $0`; pwd)
 sudo apt-get -y install build-essential asciidoc binutils bzip2 gawk gettext git libncurses5-dev libz-dev patch python3 python2.7 unzip zlib1g-dev lib32gcc1 libc6-dev-i386 subversion flex uglifyjs git-core gcc-multilib p7zip p7zip-full msmtp libssl-dev texinfo libglib2.0-dev xmlto qemu-utils upx libelf-dev autoconf automake libtool autopoint device-tree-compiler g++-multilib antlr3 gperf wget curl swig rsync &&
 
 #拉取Lean大源码
-if [ ! -d "$project_path/lede" ];then
+if [ ! -d "./lede" ];then
     echo "***正在Clone源码***"
     git clone https://github.com/coolsnowwolf/lede
     sleep 1s
@@ -34,6 +34,12 @@ if [ -d "$project_path/lede/package/otherapp" ];then
 		git clone https://github.com/destan19/OpenAppFilter  #(luci-app-flowoffload shortcut-fe luci-app-sfe)
 		#Lienol App
 		git clone https://github.com/Lienol/openwrt-package
+
+		# minieap
+		git clone https://github.com/ysc3839/openwrt-minieap.git
+		git clone https://github.com/kongfl888/minieap.git
+		git clone https://github.com/ysc3839/luci-proto-minieap.git
+		git clone https://github.com/kongfl888/luci-app-minieap.git
 		cd ../../../
 		echo "---------- $(cd `dirname $0`; pwd)"
 fi
