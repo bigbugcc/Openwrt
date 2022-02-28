ba64fbf8719d2900cabd9fcbdb887b92224d3244
bigbugcc <1373396036@qq.com>
2022-02-28 16:49:28 +0800

    rebuild code

--- openwrtinstall.sh
+++ openwrtinstall.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 project_path=$(cd `dirname $0`; pwd)
+lede_path="$project_path/lede"
 
 #编译环境
 sudo apt-get -y install build-essential asciidoc binutils bzip2 gawk gettext git libncurses5-dev libz-dev patch python3 python2.7 unzip zlib1g-dev lib32gcc1 libc6-dev-i386 subversion flex uglifyjs git-core gcc-multilib p7zip p7zip-full msmtp libssl-dev texinfo libglib2.0-dev xmlto qemu-utils upx libelf-dev autoconf automake libtool autopoint device-tree-compiler g++-multilib antlr3 gperf wget curl swig rsync &&
@@ -13,39 +14,29 @@ else
     echo "***lede源码目录已存在***"
 fi
 
-#otherapp插件目录
-if [ -d "$project_path/lede/package/otherapp" ];then
-        echo "***otherapp已存在***"
-    else
-        mkdir "$project_path/lede/package/otherapp"
-        cd $project_path/lede/package/otherapp
-		#在线用户
-		# git clone https://github.com/rufengsuixing/luci-app-onliner
-		#科学上网插件
-		git clone https://github.com/xiaorouji/openwrt-passwall
-		#git clone --depth=1 -b master https://github.com/vernesong/OpenClash
-		#大学生必备插件
-		git clone --depth=1 https://github.com/BoringCat/luci-app-mentohust
-		git clone --depth=1 https://github.com/KyleRicardo/MentoHUST-OpenWrt-ipk 
-		#广告过滤插件
-		# git clone https://github.com/rufengsuixing/luci-app-adguardhome 
-		git clone https://github.com/destan19/OpenAppFilter  #(luci-app-flowoffload shortcut-fe luci-app-sfe)
+echo "***插件下载***"
+cd $lede_path/package
+git clone  https://github.com/bigbugcc/OpenwrtApp
+git clone  https://github.com/destan19/OpenAppFilter
+git clone  https://github.com/xiaorouji/openwrt-passwall
+git clone  https://github.com/zzsj0928/luci-app-pushbot
+#替换默认主题
+rm -rf $lede_path/package/lean/luci-theme-argon
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git  $lede_path/package/lean/luci-theme-argon
 
-		git clone https://github.com/zzsj0928/luci-app-pushbot
-		#Lienol App
-		# git clone https://github.com/Lienol/openwrt-package
+echo "***修改默认配置文件***"
+cd $lede_path/
+# 修改默认IP为192.168.10.1
+sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate 
 
-		# minieap
-		# git clone https://github.com/ysc3839/openwrt-minieap.git
-		# git clone https://github.com/kongfl888/minieap.git
-		# git clone https://github.com/ysc3839/luci-proto-minieap.git
-		# git clone https://github.com/kongfl888/luci-app-minieap.git
-		cd $project_path
-fi
+# Hello World
+echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 
-cd $project_path/lede/package/lean && rm -rf luci-theme-argon 
+# luci-theme-infinityfreedom
+echo 'src-git infinityfreedom https://github.com/xiaoqingfengATGH/luci-theme-infinityfreedom.git' >>feeds.conf.default
 
-git clone https://github.com/jerrykuku/luci-theme-argon.git  
+echo "***更新安装组件***"
+./scripts/feeds update -a && ./scripts/feeds install -a
 
 cd $project_path
 
